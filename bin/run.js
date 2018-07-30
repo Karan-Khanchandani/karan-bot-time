0c61472fb9f50037c836f6886b8351d46d631eee
Karan Khanchandani <karan.khanchandani94@gmail.com>
2018-07-30 18:32:48 +0530

    Removes hardcoding of port and adds polling to server

--- bin/run.js
+++ bin/run.js
@@ -8,4 +8,12 @@ server.listen(3010);
 
 server.on('listening', function() {
     console.log(`IRIS-Time is listening on ${server.address().port} in ${service.get('env')} mode.`);
+    const announce = () => {
+        request.put(`http://127.0.0.1:3000/service/time/${server.address().port}`, (err, res) => {
+            console.log(res.body);
+            if(err) console.log("Error connecting to Iris");
+        });
+    };
+    announce();
+    setInterval(announce, 15*1000);
 });
